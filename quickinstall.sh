2950fb150a19a7d581206b3da636d3fb5e088ae5
sleepingonsoundwaves <154848161+soswav@users.noreply.github.com>
2024-01-12 21:23:27 +0000

    fuckin... fix

--- quickinstall.sh
+++ quickinstall.sh
@@ -5,7 +5,6 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 clear
-
 cd ~/
 echo -e "${GREEN}cloning git repo...${NC}"
 git clone https://github.com/soswav/bigrat.git || echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git || echo -e "${RED} didn't work, trying dnf...${NC}" && sudo dnf install git || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git
@@ -19,4 +18,5 @@ nano config.json
 echo -e "${GREEN}running bot.py...${NC}"
 python ~/bigrat/bot.py || echo -e "${RED}no python? trying to install with pacman...${NC}" && sudo pacman -S python || clear && echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install python || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install python
 
-clear && echo -e "${GREEN}bigrat discord bot succefully setup!${NC}"
+clear
+echo -e "${GREEN}bigrat discord bot succefully setup!${NC}"
