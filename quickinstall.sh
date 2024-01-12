b317951be9fe5156a31fde61e55a4a1c0997f041
sleepingonsoundwaves <154848161+soswav@users.noreply.github.com>
2024-01-12 21:36:38 +0000

    massive fix, jeez

--- quickinstall.sh
+++ quickinstall.sh
@@ -5,12 +5,14 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 clear
-cd ~/
+echo -e "${GREEN}installing dependencies for rainbow text, not required but it will look cool as hell!${NC}" && sudo pacman -S lolcat && sudo apt install lolcat && sudo dnf install lolcat
+clear
+cd ~/bigrat && echo -e "${RED}folder exists, deleting with sudo${NC}" && sudo rm -r ~/bigrat/ && cd ~/
 echo -e "${GREEN}cloning git repo...${NC}"
-git clone https://github.com/soswav/bigrat.git || echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git || echo -e "${RED} didn't work, trying dnf...${NC}" && sudo dnf install git || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git
+git clone https://github.com/soswav/bigrat.git || echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git || echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install git || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git
 
 echo -e "${GREEN}git clone finished! sending to cloned directory...${NC}"
-cd ~/bigrat || echo -e "${RED}fatal: destination path 'bigrat' already exists and is not an empty directory${NC}"
+cd ~/bigrat || echo -e "${RED}folder doesn't exist${NC}"
 
 echo -e "${GREEN}opening config.json for edit...${NC}"
 nano config.json
@@ -19,4 +21,4 @@ echo -e "${GREEN}running bot.py...${NC}"
 python ~/bigrat/bot.py || echo -e "${RED}no python? trying to install with pacman...${NC}" && sudo pacman -S python || clear && echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install python || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install python
 
 clear
-echo -e "${GREEN}bigrat discord bot succefully setup!${NC}"
+echo -e "bigrat discord bot succefully setup!" | lolcat -a
