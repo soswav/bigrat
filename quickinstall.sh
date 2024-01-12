58066713474eefd61482ac695cd945eb8c78958f
sleepingonsoundwaves <154848161+soswav@users.noreply.github.com>
2024-01-12 21:18:30 +0000

    fixed quickinstall

--- quickinstall.sh
+++ quickinstall.sh
@@ -2,22 +2,20 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-BLUE='\033[0;34m'
 NC='\033[0m'
 
+clear
+
 echo -e "${GREEN}cloning git repo...${NC}"
-if ! git clone https://github.com/soswav/bigrat.git; then
-   echo -e "${RED}no git? trying to install with pacman...${NC}"
-   sudo pacman -S git || echo -e "${RED} didn't work, trying dnf...${NC}" && sudo dnf install git || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git
-fi
+git clone https://github.com/soswav/bigrat.git ~/ || echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git || echo -e "${RED} didn't work, trying dnf...${NC}" && sudo dnf install git || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git
+
 echo -e "${GREEN}git clone finished! sending to cloned directory...${NC}"
-cd ~/bigrat
+cd ~/bigrat || echo -e "${RED}fatal: destination path 'bigrat' already exists and is not an empty directory${NC}"
 
 echo -e "${GREEN}opening config.json for edit...${NC}"
 nano config.json
 
 echo -e "${GREEN}running bot.py...${NC}"
-python ~/bigrat/bot.py
+python ~/bigrat/bot.py || echo -e "${RED}no python? trying to install with pacman...${NC}" && sudo pacman -S python || clear && echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install python || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install python
 
-echo -e "${RED}no python? trying to install with pacman...${NC}"
-sudo pacman -S python || echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install python || echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install python
+clear && echo -e "${GREEN}bigrat discord bot succefully setup!${NC}"
