8bda1b9ceb093c96ff7c0a13745a4d74aedc8af1
sleepingonsoundwaves <154848161+soswav@users.noreply.github.com>
2024-01-12 19:59:29 +0000

    fixed typo typo

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # stuff the bot needs to run, you may need to install the dependencies with pip
 # normally u can install them by running installer.bat if ur on windows
 
-import discord, time, asyncio, subproccess, os, json, logging
+import discord, time, asyncio, subprocess, os, json, logging
 from discord.ext import commands
 from discord.ext.commands import has_permissions, TextChannelConverter
 
