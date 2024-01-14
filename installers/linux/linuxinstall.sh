ee21893ba1f7b08bbecf9f75b39a65ed8e47beee
sleepingonsoundwaves <154848161+soswav@users.noreply.github.com>
2024-01-14 17:56:23 +0000

    fix

--- installers/linux/linuxinstall.sh
+++ installers/linux/linuxinstall.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+# colors for text, needed for actually reading the text lol
+RED='\033[0;31m' 
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-clear
+clear # clears the terminal so its more readeablle
 
+# asks if you want to install lolcat, y/n question if first install fails it will try with other package managers until it gets yours right
 read -p "do you want to install 'lolcat'? it's required for rainbow color in install (y/n):" choice
 case "$choice" in 
  [yY]* ) 
    echo -e "${GREEN}installing lolcat with pacman...${NC}"
-   sudo pacman -S lolcat || echo -e "${RED}didn't work, trying dnf...${NC}" && sleep 2 && sudo dnf install lolcat || echo -e "${RED}didn't work, trying apt...${NC}" && sleep 2 && sudo apt install python
+   sudo pacman -S lolcat || echo -e "${RED}didn't work, trying dnf...${NC}" && sleep 2 && sudo dnf install lolcat || echo -e "${RED}didn't work, trying apt...${NC}" && sleep 2 && sudo apt install lolcat
    ;;
  * ) 
    echo -e "${GREEN}okay, sorry for asking!${NC}"
    ;;
 esac
 
-sleep 2 && clear
-cd ~/bigrat && echo -e "${RED}folder exists, deleting with sudo${NC}" && sudo rm -r ~/bigrat/
-cd ~/
+sleep 2 && clear # takes two seconds then clears
+cd ~/bigrat && echo -e "${RED}folder exists, deleting with sudo${NC}" && sudo rm -r ~/bigrat/ # checks if you already installed bigrat
+cd  ~/
 echo -e "${GREEN}cloning git repo...${NC}"
-(git clone https://github.com/soswav/bigrat.git || (echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git || (echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install git))) || (echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git)
+git clone https://github.com/soswav/bigrat.git || (echo -e "${RED}no git? trying to install with pacman...${NC}" && sudo pacman -S git && echo -e "${GREEEN}installed! retrying...${NC}" && git clone https://github.com/soswav/bigrat.git) || (echo -e "${RED}didn't work, trying dnf...${NC}" && sudo dnf install git && echo -e "${GREEEN}installed! retrying...${NC}" && git clone https://github.com/soswav/bigrat.git) || (echo -e "${RED}didn't work, trying apt...${NC}" && sudo apt install git && echo -e "${GREEEN}installed! retrying...${NC}" && git clone https://github.com/soswav/bigrat.git)
 
-echo -e "${GREEN}git clone finished! sending to cloned directory...${NC}"
-cd ~/bigrat
+echo -e "${GREEN}git clone finished! sending to directory...${NC}"
+cd ~/bigrat # sends to directory
 
-echo -e "${GREEN}opening config.json for edit, make sure to fill the spots...${NC}"
+echo -e "${GREEN}opening config.yml for edit in 5 secs, make sure to fill the spots...${NC}"
 sleep 5
-nano ~/bigrat/bot/config.json
+nano ~/bigrat/bot/config.yml # text editor for terminals
 
 read -p "do you want to check the bot.py file? (y/n)" choice
 case "$choice" in 
@@ -45,7 +47,7 @@ read -p "do you want to run the bot right now? (y/n):" choice
 case "$choice" in 
  [yY]* ) 
    echo -e "${GREEN}running bot.py...${NC}"
-   python ~/bigrat/bot/bot.py || echo -e "${RED}no python? trying to install with pacman...${NC}" && sudo pacman -S python || echo -e "${RED}didn't work, trying dnf...${NC}" && sleep 2 && sudo dnf install python || echo -e "${RED}didn't work, trying apt...${NC}" && sleep 2 && sudo apt install python
+   python ~/bigrat/bot/bot.py || echo -e "${RED}no python? trying to install with pacman...${NC}" && sudo pacman -S python && echo -e "${GREEEN}installed! retrying...${NC}" && python ~/bigrat/bot/bot.py || echo -e "${RED}didn't work, trying dnf...${NC}" && sleep 2 && sudo dnf install python && echo -e "${GREEEN}installed! retrying...${NC}" && python ~/bigrat/bot/bot.py || echo -e "${RED}didn't work, trying apt...${NC}" && sleep 2 && sudo apt install python && echo -e "${GREEEN}installed! retrying...${NC}" && python ~/bigrat/bot/bot.py
    ;;
  * ) 
    echo -e "${GREEN}okay, later i guess...${NC}"
