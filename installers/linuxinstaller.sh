c7dc9272c4a78cfd52a51a776044c7ed14376833
soswav <154848161+soswav@users.noreply.github.com>
2024-01-31 04:58:32 +0000

    big update (feels like it)

--- installers/linuxinstaller.sh
+++ installers/linuxinstaller.sh
@@ -3,6 +3,7 @@
 # colors for text, needed for actually reading the text lol
 RED='\033[0;31m' 
 GREEN='\033[0;32m'
+ASCII='\033[1;33m' # it's yellow
 NC='\033[0m'
 
 # functions go down here, just learned these thanks to Phind.
@@ -26,13 +27,13 @@ GITFail() { # if the clone command doesn't work, it will run this
         echo -e "${GREEN}git is already installed, retrying clone...${NC}";git clone https://github.com/soswav/bigrat.git
     else # if any of the above is NOT true then it will run stuff below
         if command -v pacman > /dev/null; then
-            echo -e "${GREEN}Installing git with pacman...${NC}"; sleep 3
+            echo -e "${GREEN}installing git with pacman...${NC}";sleep 3
             sudo pacman -S git && git clone https://github.com/soswav/bigrat.git
         elif command -v dnf > /dev/null; then # if first thing  wans't true but this other thing is true then do this
-            echo -e "${GREEN}Installing git with dnf...${NC}"; sleep 3
+            echo -e "${GREEN}installing git with dnf...${NC}";sleep 3
             sudo dnf install git && git clone https://github.com/soswav/bigrat.git
         elif command -v apt > /dev/null; then
-            echo -e "${GREEN}Installing git with apt...${NC}"; sleep 3
+            echo -e "${GREEN}installing git with apt...${NC}";sleep 3
             sudo apt install git && git clone https://github.com/soswav/bigrat.git
         else
             echo -e "${RED}you don't have pacman, dnf or apt!! install git manually because i don't know what you're using!${NC}"
@@ -42,17 +43,17 @@ GITFail() { # if the clone command doesn't work, it will run this
 
 REQ() {
    if command -v pip > /dev/null; then
-        echo -e "${GREEN}pip is already installed, installing dependencies!${NC}";pip install discord requests PyYAML
+        echo -e "${GREEN}pip is already installed, installing dependencies!${NC}";pip install discord requests PyYAML asyncio
    else
        if command -v pacman > /dev/null; then
             echo -e "${RED}you don't have pip! trying to install with pacman...${NC}"; sleep 3
-            sudo pacman -S pip && pip install discord requests PyYAML
+            sudo pacman -S pip && pip install discord requests PyYAML asyncio
        elif command -v dnf > /dev/null; then
             echo -e "${RED}you don't have pip! trying to install with dnf...${NC}"; sleep 3
-            sudo dnf install pip && pip install discord requests PyYAML
+            sudo dnf install pip && pip install discord requests PyYAML asyncio
        elif command -v apt > /dev/null; then
             echo -e "${RED}you don't have pip! trying to install with dnf...${NC}"; sleep 3
-            sudo apt install pip && pip install discord requests PyYAML
+            sudo apt install pip && pip install discord requests PyYAML asyncio
        else
             echo -e "${RED}you don't have neither pacman, dnf, or apt! install pip manually you prick!${NC}"
        fi
@@ -69,9 +70,22 @@ activate_venv() {
 
     source "$env_name"/bin/activate
 }
+
+ascii(){
+    echo -e "${ASCII}
+                                                                                                               powered by soswav
+  ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗    ███████╗██╗  ██╗
+  ██║     ██║████╗  ██║██║   ██║╚██╗██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗   ██╔════╝██║  ██║
+  ██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝ ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝   ███████╗███████║
+  ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗ ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗   ╚════██║██╔══██║
+  ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║██╗███████║██║  ██║
+  ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
+                                     The big rat quick installer for computers running Linux!${NC}
+    "
+}
 # leaving funcion zone so early?
 
-clear # clears the terminal so its more readeable
+clear;ascii;sleep 3 # clears the terminal so its more readeable
 while true; do
     read -p "do you have python & pip already installed? (y/n)" choice
     case "$choice" in 
@@ -86,23 +100,23 @@ while true; do
     esac
 done
 
-sleep 3;clear
+sleep 3;clear;ascii
 # this is something you should actually read, pal!
 echo -e "${GREEN}i would like to mention that you need to toggle some stuff on discord.com/developers before continuing"
 echo -e "${GREEN}go to your bot, then search for 'Privileged Gateway Intents' on the bot tab, toggle everything you see there"
 echo -e "${RED}i'm going to make you wait 10 seconds to make sure you read all this, sorry!${NC}"; sleep 10
 
 
-clear # clears the terminal for better reability!
-cd "$HOME/bigrat" && echo -e "${RED}folder exists, deleting with sudo${NC}";rm -r -f "$HOME/bigrat/" # checks if you already installed bigrat
-sleep 1; clear;cd "$HOME" || exit # makes sure you read that, smh!
+clear;ascii # clears the terminal for better reability!
+cd "$HOME/bigrat" && echo -e "${RED}folder exists, deleting...${NC}";rm -r -f "$HOME/bigrat/" # checks if you already installed bigrat
+sleep 1; clear;ascii;cd "$HOME" || exit # makes sure you read that, smh!
 echo -e "${GREEN}cloning git repo...${NC}"; git clone https://github.com/soswav/bigrat.git || GITFail # notifies user that it's going to clone the repo, if it fails it runs the GITFail function
-clear
+clear;ascii
 
 echo -e "${GREEN}generating a venv with python...${NC}";activate_venv "$HOME/bigrat/.venv";sleep 3;clear
 echo -e "${GREEN}installing bot requirements with pip...${NC}";sleep 3;REQ
 echo -e "${GREEN}git clone, requirements installation and creation of venv finished! sending to directory...${NC}";"cd $HOME/bigrat";sleep 2 # You can guess, bucko!
-clear;echo -e "${GREEN}opening config.yml for edit in 5 secs, make sure to fill the spots...${NC}";sleep 5;nano "$HOME/bigrat/config.yml" # editor
+clear;ascii;echo -e "${GREEN}opening config.yml for edit in 5 secs, make sure to fill the spots...${NC}";sleep 5;nano "$HOME/bigrat/config.yml" # editor
 
 while true; do
     read -p "do you want to edit the bot.py file? (y/n)" choice
@@ -132,4 +146,4 @@ while true; do
     esac
 done
 
-clear;echo -e "${GREEN}bigrat discord bot succefully setup! (probably!)${NC}";sleep 2
+clear;ascii;echo -e "${GREEN}bigrat discord bot succefully setup! (probably!)${NC}";sleep 5
